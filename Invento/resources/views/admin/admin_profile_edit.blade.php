9ac7910e98eebee27ca8a3dc95fa288033d83434
Raise nil <raisulislamniloy47@gmail.com>
2023-07-28 16:45:06 +0600

    header image and name changed

--- Invento/resources/views/admin/admin_profile_edit.blade.php
+++ Invento/resources/views/admin/admin_profile_edit.blade.php
@@ -51,7 +51,7 @@
                         <div class="row mb-3 mt-3">
                             <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                             <div class="col-sm-10">
-                            <img id="showImage" class="rounded avatar-lg" alt="200x200" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                            <img id="showImage" class="rounded avatar-lg" alt="200x200" src="{{ (!empty($editData->profile_image))? url('upload/admin_images/'.$editData->profile_image):url('upload/no_image.jpg') }}" alt="Card image cap">
                             </div>
                         </div>
                         <!-- end row -->
