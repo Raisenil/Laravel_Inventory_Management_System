4a8cccf77b5fddc65537d3f0966028b881c72ccb
Raise nil <raisulislamniloy47@gmail.com>
2023-07-28 14:58:26 +0600

    Edit Profile Image jquery CDN added

--- Invento/resources/views/admin/admin_profile_edit.blade.php
+++ Invento/resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,9 @@
 @extends('admin.admin_master')
 @section('admin')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+
 <div class="page-content">
     <div class="container-fluid">
 
@@ -39,7 +42,7 @@
                         <div class="row mb-3 mt-3">
                             <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                             <div class="col-sm-10">
-                                <input name="profile_image" class="form-control" type="file" id="example-text-input">
+                                <input name="profile_image" class="form-control" type="file" id="image">
                             </div>
                         </div>
                         <!-- end row -->
@@ -47,7 +50,7 @@
                         <div class="row mb-3 mt-3">
                             <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                             <div class="col-sm-10">
-                            <img class="rounded avatar-lg" alt="200x200" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                            <img id="showImage" class="rounded avatar-lg" alt="200x200" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
                             </div>
                         </div>
                         <!-- end row -->
@@ -63,5 +66,18 @@
 
     </div>
 </div>
+
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 
 @endsection
