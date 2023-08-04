f35e5333bf3eb46f32b5729dfbc7c665e1c42e2e
Raise nil <raisulislamniloy47@gmail.com>
2023-08-04 15:18:13 +0600

    password update done

--- Invento/routes/web.php
+++ Invento/routes/web.php
@@ -32,6 +32,10 @@ Route::controller(AdminController::class)->group(Function(){
     Route::get('/edit/profile','EditProfile')->name('edit.profile');
 
     Route::post('/store/profile','StoreProfile')->name('store.profile');
+
+    Route::get('/change/password','ChangePassword')->name('change.password');
+
+    Route::post('/update/password','UpdatePassword')->name('update.password');
 });
 
 
