3af18c296810b72ff5bfd279a143634608622c7e
Raise nil <raisulislamniloy47@gmail.com>
2023-09-16 20:34:20 +0600

    Custom validation added

--- Invento/app/Http/Controllers/Home/PortfolioController.php
+++ Invento/app/Http/Controllers/Home/PortfolioController.php
@@ -19,6 +19,14 @@ class PortfolioController extends Controller
     }
 
     public function StorePortfolio(Request $request){
-        
+        $request->validate([
+            'portfolio_name' => 'required',
+            'portfolio_title' => 'required',
+            'portfolio_description' => 'required',
+            'portfolio_image' => 'required',
+        ],[
+            'portfolio_name.required' => 'Portfolio Name is Required',
+            'portfolio_title.required' => 'Portfolio Title is Required',
+        ]);
     }
 }
