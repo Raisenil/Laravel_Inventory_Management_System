969e9219ce0a3ddbb404ccd4c1caaf78c0ccc3b7
Raise nil <raisulislamniloy47@gmail.com>
2023-09-20 21:02:56 +0600

    Blog Sidebar added

--- Invento/resources/views/admin/body/sidebar.blade.php
+++ Invento/resources/views/admin/body/sidebar.blade.php
@@ -58,6 +58,17 @@
                                     <span>Blog Category</span>
                                 </a>
                                 <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="{{route('all.blog.category')}}">All Blog Category</a></li>
+                                    <li><a href="{{route('add.blog.category')}}">Add Blog Category</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="ri-layout-3-line"></i>
+                                    <span>Blog Page</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="false">
                                     <li><a href="{{route('all.blog.category')}}">All Blog</a></li>
                                     <li><a href="{{route('add.blog.category')}}">Add Blog</a></li>
                                 </ul>
