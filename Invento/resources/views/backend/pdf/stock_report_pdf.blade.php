2197307d93d0a84a946727820f6da1f40ade6a18
Raise nil <raisulislamniloy47@gmail.com>
2023-10-17 10:49:00 +0600

    Stock report table updated

--- Invento/resources/views/backend/pdf/stock_report_pdf.blade.php
+++ Invento/resources/views/backend/pdf/stock_report_pdf.blade.php
@@ -59,6 +59,8 @@
                                                     <td class="text-center"><strong>Category</strong>
                                                     </td>
                                                     <td class="text-center"><strong>Product Name</strong>
+                                                    <td class="text-center"><strong>In Qty</strong>
+                                                    <td class="text-center"><strong>Out Qty</strong>
                                                     </td>
                                                     <td class="text-center"><strong>Stock</strong>
                                                     </td>
@@ -67,12 +69,19 @@
                                                 <tbody>
                                                 <!-- foreach ($order->lineItems as $line) or some such thing here -->
                                                 @foreach($allData as $key => $item)
+                                                @php
+                                                $buying_total = App\Models\Purchase::where('category_id',$item->category_id)->where('product_id',$item->id)->where('status','1')->sum('buying_qty');
+
+                                                $selling_total = App\Models\InvoiceDetail::where('category_id',$item->category_id)->where('product_id',$item->id)->where('status','1')->sum('selling_qty');
+                                                @endphp
                                                 <tr>
                                                     <td class="text-center">{{( $key+1 )}}</td>
                                                     <td class="text-center">{{( $item['supplier']['name'] )}}</td>
                                                     <td class="text-center">{{( $item['unit']['name'] )}}</td>
                                                     <td class="text-center">{{( $item['category']['name'] )}}</td>
                                                     <td class="text-center">{{( $item->name )}}</td>
+                                                    <td class="text-center">{{( $buying_total )}}</td>
+                                                    <td class="text-center">{{( $selling_total )}}</td>
                                                     <td class="text-center">{{( $item->quantity )}}</td>
                                                 </tr>
                                                 @endforeach
